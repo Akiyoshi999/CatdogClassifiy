3fde69cad300405a8354997b1b809954d5b96ae4
Akiyoshi <zundokokiyoshi999@gmail.com>
2020-11-25 22:01:08 +0900

    README更新

--- catdogclassifiy/catdog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CatdogConfig(AppConfig):
-    name = 'catdog'
--- catdogclassifiy/catdogclassifiy/views.py
+++
@@ -1,6 +0,0 @@
-from django.http.response import HttpResponse
-
-
-def helloworldfunction(request):
-    returnobject = HttpResponse('hello world')
-    return returnobject
